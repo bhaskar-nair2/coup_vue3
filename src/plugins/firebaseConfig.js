d5ead2f67db7e080ed419acced5b519fd524ef73
bhaskar <b.bhaskar.nair@gmail.com>
2020-11-17 21:30:53 +0530

    firest commit

---
+++ src/plugins/firebaseConfig.js
@@ -0,0 +1,14 @@
+import app from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDz6C0Oq-PtAbkjnB7JAhJRW1Be5-XOrmM",
+  authDomain: "coup-dc26b.firebaseapp.com",
+  databaseURL: "https://coup-dc26b.firebaseio.com",
+  projectId: "coup-dc26b",
+  storageBucket: "coup-dc26b.appspot.com",
+  messagingSenderId: "457763320714",
+  appId: "1:457763320714:web:992598453ed003f6bf0e73",
+  measurementId: "G-FW11G1ZPZM"
+};
+
+app.initializeApp(firebaseConfig);
